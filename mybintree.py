00dc1690bd8212b6e802e48cc09d3cc167a8c07b
eycycyc <justin.y.chen@ericsson.com>
2018-01-18 18:36:45 +0800

    2nd note

--- mybintree.py
+++ mybintree.py
@@ -6,9 +6,11 @@ class Node(object):
         self.right = None
 
 class Tree(object):
-    def __init__(self, height=0):
+    def __init__(self):
         self.root = Node()
         self.height = 0
+        self.result = []
+
     def build_bst_from_sorted_values_middle(self, sorted_values):
         if len(sorted_values) == 0:
             return None
@@ -24,21 +26,25 @@ class Tree(object):
     def travel_xianxu(self, root):
         if root == None:
            return
-        print root.val
+        self.result.append(root.val)
         self.travel_xianxu(root.left)
         self.travel_xianxu(root.right)
+        return self.result
     def travel_houxu(self,root):
         if root == None:
            return
         self.travel_houxu(root.left)
         self.travel_houxu(root.right)
-        print root.val
+        self.result.append(root.val)
+        return self.result
     def travel_zhongxu(self,root):
         if root == None:
            return
-        self.travel_zhongxu(root.left)
-        print root.val
-        self.travel_zhongxu(root.right)
+        self.__travel_zhongxu(root.left)
+        self.result.append(root.val)
+        self.__travel_zhongxu(root.right)
+        return self.result
+
     def contains(self,root,num):
         if root == None:
            return False
@@ -58,17 +64,20 @@ class Tree(object):
         return max(self.getheight(root.left), self.getheight(root.right)) + 1
 
 def main():
-    values = [1,2,3,4,4,6,7,7,7,8,8,8,9,9,10,11,12,13,14,15]
+    values = range(65)
     tree_mid = Tree()
     treeroot = tree_mid.build_bst_from_sorted_values_middle(values)
+    print tree_mid.travel_xianxu(treeroot)
+#    print tree_mid.travel_houxu(treeroot)
+#    print tree_mid.travel_zhongxu(treeroot)
 
     def travel():
         print "xianxu"
-        tree_mid.travel_xianxu(treeroot)
+        print tree_mid.travel_xianxu(treeroot)
         print "houxu"
-        tree_mid.travel_houxu(treeroot)
+        print tree_mid.travel_houxu(treeroot)
         print "zhongxu"
-        tree_mid.travel_zhongxu(treeroot)
+        print tree_mid.travel_zhongxu(treeroot)
 
     def ifcontains():
         for value in values:
@@ -83,7 +92,7 @@ def main():
     def checkproperitity():   
         print "The height of the Tree is " + str(tree_mid.getheight(treeroot))
 
-    travel()
+    #travel()
     #ifcontains()
     checkproperitity()
 
